a3aefa877491951f9ee4a9d32ee6ce4aaa123da3
0023jas <sandersonjack02@gmail.com>
2023-10-06 23:16:12 +0100

    added text for the projects

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 console.log("hello")
 
 document.getElementById('project-1').addEventListener('click', function() {
-    document.getElementById('projects-info').innerText = 'Content changed for project 1!';
+    document.getElementById('projects-info').innerText = 'A program that takes an ASCII image of an anime girl and converts them into a 256x256 color picture using the PyTorch CycleGAN library. ';
 });
 
 document.getElementById('project-2').addEventListener('click', function() {
