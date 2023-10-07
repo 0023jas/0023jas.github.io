9d640efc1ccf55b9cbb5b11c7d983f220ff4210a
0023jas <sandersonjack02@gmail.com>
2023-10-07 23:42:21 +0100

    added more details to projects

--- projects.js
+++ projects.js
@@ -1,13 +1,13 @@
 console.log("hello")
 
 document.getElementById('project-1').addEventListener('click', function() {
-    document.getElementById('projects-info').innerText = 'A program that takes an ASCII image of an anime girl and converts them into a 256x256 color picture using the PyTorch CycleGAN library. ';
+    document.getElementById('projects-info').innerText = 'A program that takes an ASCII image of an anime girl and converts them into a 256x256 color picture using the PyTorch CycleGAN library.';
 });
 
 document.getElementById('project-2').addEventListener('click', function() {
-    document.getElementById('projects-info').innerText = 'Content changed for project 2 woo hoo!';
+    document.getElementById('projects-info').innerText = "My bachelor's thesis that seeks to find the impact of an update on the Ethereum Blockchain.";
 });
 
 document.getElementById('project-3').addEventListener('click', function() {
-    document.getElementById('projects-info').innerText = 'Content changed for project 3 boo doo doo!';
+    document.getElementById('projects-info').innerText = 'A program used to track the ISS, and other cool NASA and ESA projects.';
 });
